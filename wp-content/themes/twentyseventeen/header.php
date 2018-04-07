9dce05462fb2ff5f61a23b1772748b186a774e8a
Admin <info@socialcyclinglex.org>
2018-04-07 05:27:13 -0400

    Weekly backup - April 7, 2018

--- wp-content/themes/twentyseventeen/header.php
+++ wp-content/themes/twentyseventeen/header.php
@@ -18,7 +18,6 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="stylesheet" href="https://use.typekit.net/pim0abt.css">
 
 <?php wp_head(); ?>
 </head>
